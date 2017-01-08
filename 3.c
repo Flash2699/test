b987142b528dd3719e263f929d184d5a1c731f3b
flash2699 <palasha84@gmail.com>
2017-01-08 19:21:34 +0530

    .

---
+++ 3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+printf("Enter 1st num:");
+scanf("%d", &a);
+printf("Enter 2nd num:");
+scanf("%d", &b);
+c=a+b;
+printf("Addition is %d:",c);
+getch();
+}
